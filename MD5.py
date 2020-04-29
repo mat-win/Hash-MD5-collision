a34aa56e128e11444af1219de34510116c29b760
Mateusz Winnicki <mateusz_winnicki@o2.pl>
2020-04-29 19:56:59 +0200

    method from py2 to py3

--- MD5.py
+++ MD5.py
@@ -1,11 +1,16 @@
-import hashlib
+from hashlib import md5
 
 
-my_begin = str(input("Enter the beginnning of the message: "))
+#my_begin = str(input("Enter the beginnning of the message: "))
+my_begin = b'\x42\x66\x37'
 
 def double_md5(message, x, begin):
   print(message, x, begin)
-  return hashlib.md5(hashlib.md5(begin + message).digest()).hexdigest()[:x]
+  return md5(md5(begin + message).digest()).digest()[:x]
+
+def double_md5_II(message, x, begin):
+  # print(message, x, begin)
+  return md5(md5(begin + message).digest()).hexdigest()[:x*2]
 
 def floyd(x, initial):
 
@@ -14,36 +19,51 @@ def floyd(x, initial):
   m1 = None
 
 
-  slow = double_md5(x0, x, my_begin)
-  fast = double_md5(slow, x, my_begin)
+  turtle = double_md5(x0, x, my_begin)
+  hare = double_md5(turtle, x, my_begin)
 
-  while slow != fast:
-    slow = double_md5(slow, x, my_begin)
-    fast   = double_md5(double_md5(fast, x, my_begin), x, my_begin)
+  while turtle != hare:
+    turtle = double_md5(turtle, x, my_begin)
+    hare   = double_md5(double_md5(hare, x, my_begin), x, my_begin)
 
-  slow = x0
+  turtle = x0
   
-  while slow != fast:
-    m0 = slow
-    slow = double_md5(slow, x, my_begin)
-    fast = double_md5(fast, x, my_begin)
+  while turtle != hare:
+    m0 = turtle
+    turtle = double_md5(turtle, x, my_begin)
+    hare = double_md5(hare, x, my_begin)
 
 
-  fast = double_md5(slow, x, my_begin)
+  hare = double_md5(turtle, x, my_begin)
 
-  while slow != fast:
-    m1 = fast
-    fast = double_md5(fast, x, my_begin)
+  while turtle != hare:
+    m1 = hare
+    hare = double_md5(hare, x, my_begin)
+
+  print_results(m0, m1, double_md5_II(m0, x, my_begin))
+  print_results_hex(m0, m1, double_md5_II(m0, x, my_begin))
 
 
-
-  print_results(m0, m1, double_md5(m0, x, my_begin))
-
+#wynik w  bajtach
 
 def print_results(m0, m1, hash):
-  print "First message:", my_begin + m0
-  print "Second message:", my_begin + m1
-  print "Hash of both:", hash
+  print('___________________________________')
+  print('Bytes results: ')
+  print('First message:', my_begin + m0)
+  print('Second message:', my_begin + m1)
+  print('Hash of both:', hash)
 
-x = input("How many collisions bytes you need?: ")
-floyd(x, "123456789xyz")
+
+#wynik w hexach
+
+def print_results_hex(m0, m1, hash):
+  print('___________________________________')
+  print('Hex results: ')
+  print('First message:', (my_begin + m0).hex())
+  print('Second message:', (my_begin + m1).hex())
+  print('Hash of both:', hash)
+
+x = int(input('How many collisions bytes you need?: '))
+floyd(x, b'123456789xyz')
+
+
